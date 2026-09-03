3d19d64a7041c38de12fcec895102147a7a75d9f
rohit <royyo8226@gmail.com>
2026-09-03 11:40:08 +0530

    working with models

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.on("error", (err) => {
-        console.error("Failed to connect to the database:", err);
-        process.exit(1);
-    })
-    app.listen(process.env.PORT||8000, () => {  
+    const server = app.listen(process.env.PORT || 8000, () => {
+        console.log(
+            "Connected to the database successfully.",
+            process.env.PORT || 8000
+        );
+    });
 
-    console.log("Connected to the database successfully.",process.env.PORT||8000);
-})})
+    server.on("error", (err) => {
+        console.error("Server error:", err);
+        process.exit(1);
+    });
+})
 .catch((err) => {
     console.error("Failed to connect to the database:", err);
     process.exit(1);
-})
+});
